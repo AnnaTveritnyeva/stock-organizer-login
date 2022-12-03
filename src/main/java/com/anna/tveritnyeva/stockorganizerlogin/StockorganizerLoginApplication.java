48e818acf37278e5c15a755a2b619381427afb4c
AnnaTveritnyeva <atveretniv@gmail.com>
2022-12-03 19:11:52 +0200

    Initial commit

---
+++ src/main/java/com/anna/tveritnyeva/stockorganizerlogin/StockorganizerLoginApplication.java
@@ -0,0 +1,13 @@
+package com.anna.tveritnyeva.stockorganizerlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockorganizerLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockorganizerLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anna/tveritnyeva/stockorganizerlogin/StockorganizerLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anna.tveritnyeva.stockorganizerlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockorganizerLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
